c498f84511099ebb5cafce0866e4f9273bfc0bd8
Atul Madhugiri <atulm@berkeley.edu>
2021-01-04 12:12:15 -0800

    copy/paste images PNG/JPG iOS JS

--- src/Clipboard.ts
+++ src/Clipboard.ts
@@ -27,6 +27,47 @@ export const Clipboard = {
   setString(content: string) {
     NativeClipboard.setString(content);
   },
+  
+  
+  
+  
+  /**
+   * Get content of image type as PNG, this method returns a `Promise`, so you can use following code to get clipboard content
+   * ```javascript
+   * async _getContent() {
+   *   var content = await Clipboard.getImagePNG();
+   * }
+   * ```
+   */
+  getImagePNG(): Promise<string> {
+    return NativeClipboard.getImagePNG();
+  },
+  
+  /**
+   * Get content of image type as JPG, this method returns a `Promise`, so you can use following code to get clipboard content
+   * ```javascript
+   * async _getContent() {
+   *   var content = await Clipboard.getImageJPG();
+   * }
+   * ```
+   */
+  getImageJPG(): Promise<string> {
+    return NativeClipboard.getImageJPG();
+  },
+  /**
+   * Set content of image type. You can use following code to set clipboard content
+   * ```javascript
+   * _setContent() {
+   *   Clipboard.setImage('data:image/png;base64,....');
+   * }
+   * ```
+   * @param the content to be stored in the clipboard.
+   */
+  setImage(image: string) {
+    NativeClipboard.setImage(image);
+  },
+  
+  
   /**
    * Returns whether the clipboard has content or is empty.
    * This method returns a `Promise`, so you can use following code to get clipboard content
